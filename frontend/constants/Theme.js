9e0c7bc41cb4aef459b2643c0de93375f444fd78
Suborajeet-Das <subrodazz@gmail.com>
2025-12-01 22:39:30 +0530

    screens with different role folders

---
+++ frontend/constants/Theme.js
@@ -0,0 +1,27 @@
+export const COLORS = {
+  primary: '#81122F',
+  secondary: "#2F1018",
+
+  blur: '#e7e5e5ff',
+  blurDark: '#111111',
+
+  gray: "#777",
+  lightGray: "#969696ff",
+
+  gradient1: ['#0E0E10','#3D8BFF', '#0E0E10'],
+  gradient2: ['#0E0E10','#3D8BFF', '#B83AFF', '#0E0E10'],
+
+};
+
+export const SIZE = {
+  xsmall: 10,
+  small: 12,
+  medium: 14,
+  large: 16,
+  xlarge: 20,
+  h2: 24,
+  h1: 32,
+};
+
+
+
